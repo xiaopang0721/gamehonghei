1f966407f10c1a69f86056965c9fae5255e11969
happy <happy>
2019-09-16 21:32:04 +0800

    client批量提交

--- page/HongheiMapPage.ts
+++ page/HongheiMapPage.ts
@@ -115,7 +115,8 @@ module gamehonghei.page {
             }
             this._viewUI.mouseThrough = true;
             this._game.playMusic(Path_game_honghei.music_honghei + "honghei_bgm.mp3");
-            this._viewUI.btn_spread.left = this._game.isFullScreen ? 25 : 10;
+            this._viewUI.btn_spread.left = this._game.isFullScreen ? 30 : 10;
+            this._viewUI.box_menu.left = this._game.isFullScreen ? 25 : 10;
         }
 
         // 页面打开时执行函数
