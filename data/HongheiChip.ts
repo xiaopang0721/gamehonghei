be9942095a58656927b27b893f6a2c29c371a6d2
xiaoer <xiaoer@example.com>
2019-12-07 22:33:05 +0800

    筹码修改

--- data/HongheiChip.ts
+++ data/HongheiChip.ts
@@ -24,7 +24,7 @@ module gamehonghei.data {
 			this._type = type;
 			this._startIndex = startIdx;
 			this._targetIndex = targetIdx - 1;
-			this.rotateAngle = MathU.randomRange(0, 360);
+			// this.rotateAngle = MathU.randomRange(0, 360);
 			this._seatIndex = unitIndex;
 			this._radiusX = targetIdx <= 2 ? 100 : 150;
 			this._radiusY = targetIdx <= 2 ? 95 : 90;
@@ -62,11 +62,7 @@ module gamehonghei.data {
 			this.targe_pos.x = target[index][0];
 			this.targe_pos.y = target[index][1];
 			if (!this.pos) return;
-			super.comebackChip();
-			Laya.Tween.to(this.pos, { x: this.targe_pos.x, y: this.targe_pos.y }, 500 + count * 15, Laya.Ease.backIn, Handler.create(this, () => {
-				this.isFinalPos = true;
-				game.sceneObjectMgr.clearOfflineObject(this);
-			}));
+			super.flyChipBase(500 + count * 15,game);
 		}
 	}
 }
